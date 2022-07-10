fff8ac4c8da3ab668634bbe141ab7f69565edeb6
Craig Pastro <pastro.craig@gmail.com>
2022-07-10 13:23:39 -0700

    Do some Go

---
+++ go/easy/21_merge_two_sorted_lists.go
@@ -0,0 +1,27 @@
+// 21. Merge Two Sorted Lists
+// Easy
+//
+// Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+
+	if list1.Val <= list2.Val {
+		list1.Next = mergeTwoLists(list1.Next, list2)
+		return list1
+	}
+
+	list2.Next = mergeTwoLists(list1, list2.Next)
+	return list2
+}
