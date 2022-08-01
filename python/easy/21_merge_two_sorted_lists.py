102d30f7cf0711baddeada50c1151198b7b718ea
Craig Pastro <pastro.craig@gmail.com>
2022-08-01 09:31:44 +0900

    more

---
+++ python/easy/21_merge_two_sorted_lists.py
@@ -0,0 +1,24 @@
+# 21. Merge Two Sorted Lists
+# Easy
+#
+# Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if list1 == None:
+            return list2
+        
+        if list2 == None:
+            return list1
+      
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
