ceb411e66e160959f5a49bb9251161dab012b304
Craig Pastro <pastro.craig@gmail.com>
2022-08-07 12:27:44 +0900

    couple more

---
+++ python/easy/219_contains_duplicate_ii.py
@@ -0,0 +1,20 @@
+"""
+219. Contains Duplicate II
+Easy
+
+Given an integer array nums and an integer k, return true if there are two
+distinct indices i and j in the array such that nums[i] == nums[j] and
+abs(i - j) <= k.
+"""
+
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        d = {}
+
+        for i, num in enumerate(nums):
+            if num in d and i - d[num] <= k:
+                return True
+            d[num] = i
+
+        return False
