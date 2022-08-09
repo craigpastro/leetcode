49946a224b681fd43ae6c5ab709ce1e07d1293cb
Craig Pastro <pastro.craig@gmail.com>
2022-08-09 12:32:21 +0900

    Do some more Python ones (#9)

---
+++ python/easy/349_intersection_of_two_arrays.py
@@ -0,0 +1,22 @@
+"""
+349. Intersection of Two Arrays
+Easy
+
+Given two integer arrays nums1 and nums2, return an array of their
+intersection. Each element in the result must be unique and you may return
+the result in any order.
+"""
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        d = set()
+        for num in set(nums1):
+            d.add(num)
+
+        res = []
+        for num in set(nums2):
+            if num in d:
+                res.append(num)
+
+        return res
---
+++ python/easy/350_intersection_of_two_arrays_2.py
@@ -0,0 +1,26 @@
+"""
+350. Intersection of Two Arrays II
+Easy
+
+Given two integer arrays nums1 and nums2, return an array of their
+intersection. Each element in the result must appear as many times as it
+shows in both arrays and you may return the result in any order.
+"""
+
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        d = {}
+        for num in nums1:
+            if num in d:
+                d[num] += 1
+            else:
+                d[num] = 1
+
+        res = []
+        for num in nums2:
+            if num in d and d[num] > 0:
+                res.append(num)
+                d[num] -= 1
+
+        return res
