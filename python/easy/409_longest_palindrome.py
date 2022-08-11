53714e356b5233a46af7404ae3e8e0ece26d1261
Craig Pastro <pastro.craig@gmail.com>
2022-08-11 17:05:19 +0900

    do some more

---
+++ python/easy/409_longest_palindrome.py
@@ -0,0 +1,31 @@
+"""
+409. Longest Palindrome
+Easy
+
+Given a string s which consists of lowercase or uppercase letters, return
+the length of the longest palindrome that can be built with those letters.
+
+Letters are case sensitive, for example, "Aa" is not considered a
+palindrome here.
+"""
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        d = {}
+        for c in s:
+            if c in d:
+                d[c] += 1
+            else:
+                d[c] = 1
+
+        res = 0
+        rem = 0
+        for v in d.values():
+            if v % 2 == 0:
+                res += v
+            else:
+                res += v - 1
+                rem = 1
+
+        return res + rem
