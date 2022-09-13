9efe9d3a720d5a376ec09f58ac1082f005c29c2f
Craig Pastro <pastro.craig@gmail.com>
2022-09-12 19:24:06 -0700

    Do some more with Python (#18)

---
+++ python/415_add_strings.py
@@ -0,0 +1,36 @@
+"""
+415. Add Strings
+Easy
+
+Given two non-negative integers, num1 and num2 represented as string, return
+the sum of num1 and num2 as a string.
+
+You must solve the problem without using any built-in library for handling
+large integers (such as BigInteger). You must also not convert the inputs to
+integers directly.
+"""
+
+
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        l1 = len(num1)
+        l2 = len(num2)
+        if l1 <= l2:
+            num1 = num1.zfill(l2)
+        else:
+            num2 = num2.zfill(l1)
+
+        num1 = num1[::-1]
+        num2 = num2[::-1]
+
+        res = ""
+        carry = 0
+        for i in range(len(num1)):
+            temp = int(num1[i]) + int(num2[i]) + carry
+            carry = temp // 10
+            res += str(temp % 10)
+
+        if carry > 0:
+            res += str(carry)
+
+        return res[::-1]
---
+++ python/461_hamming_distance.py
@@ -0,0 +1,14 @@
+"""
+461. Hamming Distance
+Easy
+
+The Hamming distance between two integers is the number of positions at
+which the corresponding bits are different.
+
+Given two integers x and y, return the Hamming distance between them.
+"""
+
+
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        return format(x ^ y, "b").count("1")
