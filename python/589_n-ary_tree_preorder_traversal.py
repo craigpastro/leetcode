45f4658eefe98b31df705acb346a2c64fc8702b8
Craig Pastro <pastro.craig@gmail.com>
2022-09-20 08:24:35 -0700

    yep

--- python/589_n-ary_tree_preorder_traversal.py
+++ python/589_n-ary_tree_preorder_traversal.py
@@ -4,9 +4,6 @@ Easy
 
 Given the root of an n-ary tree, return the preorder traversal of its nodes'
 values.
-
-Nary-Tree input serialization is represented in their level order traversal.
-Each group of children is separated by the null value (See examples)
 """
 
 
---
+++ python/590_n-ary_tree_postorder_traversal.py
@@ -0,0 +1,25 @@
+"""
+590. N-ary Tree Postorder Traversal
+Easy
+
+Given the root of an n-ary tree, return the postorder traversal of its nodes'
+values.
+"""
+
+
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+    def postorder(self, root: "Node") -> List[int]:
+        if root is None:
+            return []
+
+        res = []
+        for child in root.children:
+            res.extend(self.postorder(child))
+
+        return res + [root.val]
