3cd2a228a561ea39407cec3ed772049437ae5450
Craig Pastro <pastro.craig@gmail.com>
2022-10-13 10:06:04 -0700

    more

---
+++ python/925_long_pressed_name.py
@@ -0,0 +1,26 @@
+"""
+925. Long Pressed Name
+Easy
+
+Your friend is typing his name into a keyboard. Sometimes, when typing a
+character c, the key might get long pressed, and the character will be typed
+1 or more times.
+
+You examine the typed characters of the keyboard. Return True if it is
+possible that it was your friends name, with some characters (possibly none)
+being long pressed.
+"""
+
+
+class Solution:
+    def isLongPressedName(self, name: str, typed: str) -> bool:
+        n = len(name)
+        i = 0
+
+        for j in range(len(typed)):
+            if i < n and name[i] == typed[j]:
+                i += 1
+            elif j == 0 or typed[j] != typed[j - 1]:
+                return False
+
+        return i == n
