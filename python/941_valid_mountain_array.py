58c614bcce86d779d26e64825d1491ae3c9f57ef
Craig Pastro <pastro.craig@gmail.com>
2022-10-13 11:14:44 -0700

    few more

---
+++ python/941_valid_mountain_array.py
@@ -0,0 +1,40 @@
+"""
+941. Valid Mountain Array
+Easy
+
+Given an array of integers arr, return true if and only if it is a valid
+mountain array.
+
+Recall that arr is a mountain array if and only if:
+
+- arr.length >= 3
+- There exists some i with 0 < i < arr.length - 1 such that:
+  - arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+  - arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+"""
+
+
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        n = len(arr)
+
+        if n < 3:
+            return False
+
+        i = 1
+        while i < n:
+            if arr[i - 1] < arr[i]:
+                i += 1
+            else:
+                break
+
+        if i == 1 or i == n:
+            return False  # need both an increasing and decreasing part
+
+        while i < n:
+            if arr[i - 1] > arr[i]:
+                i += 1
+            else:
+                return False
+
+        return i == n
