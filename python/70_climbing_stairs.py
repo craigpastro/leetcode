b522a04c210669a263e781256bfa34d5199800d8
Craig Pastro <pastro.craig@gmail.com>
2022-11-24 17:08:50 -0800

    Do some more

--- python/70_climbing_stairs.py
+++ python/70_climbing_stairs.py
@@ -8,17 +8,18 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can
 you climb to the top?
 """
 
-d = {1: 1, 2: 2}
-
 
 class Solution:
+    def __init__(self):
+        self.d = {1: 1, 2: 2}
+
     def climbStairs(self, n: int) -> int:
-        if n in d:
-            return d[n]
+        if n in self.d:
+            return self.d[n]
 
-        if n - 1 not in d:
-            d[n - 1] = self.climbStairs(n - 1)
-        if n - 2 not in d:
-            d[n - 2] = self.climbStairs(n - 2)
+        if n - 1 not in self.d:
+            self.d[n - 1] = self.climbStairs(n - 1)
+        if n - 2 not in self.d:
+            self.d[n - 2] = self.climbStairs(n - 2)
 
-        return d[n - 1] + d[n - 2]
+        return self.d[n - 1] + self.d[n - 2]
